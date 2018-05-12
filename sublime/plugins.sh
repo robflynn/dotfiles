e212bea0118593d5226a682962123cccd715fecc
Rob Flynn <rob@thingerly.com>
2018-05-12 18:34:21 -0400

    Add default keybindingds and sublime packages

---
+++ sublime/plugins.sh
@@ -0,0 +1,6 @@
+wget https://packagecontrol.io/Package%20Control.sublime-package
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+cp "Package Control.sublime-package" ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+
+cp -rf Library ~/
+
